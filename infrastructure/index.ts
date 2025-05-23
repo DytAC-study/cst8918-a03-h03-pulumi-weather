b77aceb5b033b93719229911ca76a7ed3f57d74a
DytAC-study <du000086@algonquinlive.com>
2025-05-22 20:44:32 -0400

    updated redis-connection.ts, index.ts), open-weather-service.ts and ran local test.

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -100,7 +100,7 @@ const containerGroup = new containerinstance.ContainerGroup(
           },
           {
             name: 'WEATHER_API_KEY',
-            value: config.requireSecret('weatherApiKey'),
+            value: config.requireSecret('weatherApiKey')
           },
         ],
         resources: {
